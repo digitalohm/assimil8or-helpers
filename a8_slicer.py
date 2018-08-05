d5640debd88bf1284ea963a03d0f07f73076786f
digitalohm <>
2018-08-05 11:02:14 -0700

    Added slicing script
    added a very rudimentary script for generating the yml for 8 slices per zone.

---
+++ a8_slicer.py
@@ -0,0 +1,25 @@
+import wave
+import contextlib
+
+audiofile = 'Seraphim.wav' 
+
+with contextlib.closing(wave.open(audiofile,'r')) as f: 
+  frames = f.getnframes()
+  rate = f.getframerate()
+  length = frames / float(rate)    
+  sample_length = int(round(length * frames))
+  division = int(round(sample_length/8))
+  print(sample_length)
+  print(division)
+
+count = 0
+part = 0
+
+while (count < 8):
+    print('Zone ' + str((count+1)) + ':')
+    print('  Sample : ' + audiofile)
+    print('  SampleStart : ' + str(part))
+    print('  SampleEnd : ' + str(part+division))
+    #print(part, (part + division)
+    part = (part + division + 1)
+    count = count + 1
