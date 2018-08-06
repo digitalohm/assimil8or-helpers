a1591a50103caf4a259150a5ac599530f504431f
digitalohm <>
2018-08-05 17:49:23 -0700

    Fixed Sample Countj
    I had an error previously with sample count.  That is fixed using the frames value.  I also added a function for formatting the numbers with preceding zeroes

--- a8_slicer.py
+++ a8_slicer.py
@@ -1,6 +1,11 @@
 import wave
 import contextlib
 
+## The following function formats a number so that it has leading 0s as needed for the sample start/end values
+def formatnumber(format_me):
+    if len(format_me) < 8:
+        return format_me.zfill(8 - len(format_me) + len(format_me))
+
 sample_name = 'loop001.wav'
 
 ## The below psuedo voltage table is for the XOR NerdSeq starting at note C-4 and moving up per note
@@ -11,9 +16,7 @@ with contextlib.closing(wave.open(sample_name,'r')) as f:
   rate = f.getframerate()
   length = frames / float(rate)
   sample_length = int(round(length * frames))
-  division = int(round(sample_length/8))
-  #print(sample_length)
-  #print(division)
+  division = int(round(frames/8))
 
 count = 0
 part = 0
@@ -21,8 +24,8 @@ part = 0
 while (count < 8):
     print('Zone ' + str((count+1)) + ':')
     print('  Sample : ' + sample_name)
-    print('  SampleStart : ' + str(part))
-    print('  SampleEnd : ' + str(part + division))
+    print('  SampleStart : ' + formatnumber(str(part)))
+    print('  SampleEnd : ' + formatnumber(str(part + division)))
     print('  MinVoltage : ' + vt_nerdseq_001[count])
     part = (part + division + 1)
     count = count + 1
