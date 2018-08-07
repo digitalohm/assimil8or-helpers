9b02498b437bbc223ead0da2d05484777c49c272
digitalohm <>
2018-08-06 22:16:00 -0700

    updated paths
    updated paths and tested more projectrs

--- a8_exporter.py
+++ a8_exporter.py
@@ -1,10 +1,10 @@
 import os, shutil, glob
 
-project_dir = "prst001"
+project_dir = 'prst005'
 
 source_dir = '/Users/digitalohm/Documents/Ableton/' + project_dir + ' Project/Samples/Imported'
-temp_dir = "/Users/digitalohm/Documents/temp_samples"
-dest_dir = "/Users/digitalohm/Documents/test_samples"
+temp_dir = '/Users/digitalohm/Documents/temp_samples'
+dest_dir = '/Users/digitalohm/Documents/' + project_dir
 sub_dir = ['1', '2', '3', '4', '5', '6', '7', '8']
 
 if not os.path.exists(temp_dir):
@@ -21,19 +21,19 @@ for subdirectory in sub_dir:
 for root, dirnames, filenames in os.walk(source_dir):
     for file in filenames:
         (shortname, extension) = os.path.splitext(file)
-        if extension == ".wav" :
+        if extension == '.wav' :
             shutil.copy2(os.path.join(root,file), os.path.join(temp_dir, os.path.relpath(os.path.join(root,file),source_dir)))
 
 for filename in os.listdir(temp_dir):
      (shortname, extension) = os.path.splitext(filename)
-     if extension == ".wav" :
+     if extension == '.wav' :
          prefix = project_dir + '_'
          os.rename(os.path.join(temp_dir, filename), os.path.join(temp_dir, prefix + filename))
 
 for root, dirnames, filenames in os.walk(temp_dir):
     for file in filenames:
         (shortname, extension) = os.path.splitext(file)
-        if extension == ".wav" :
+        if extension == '.wav' :
             shutil.copy2(os.path.join(root,file), os.path.join(dest_dir, os.path.relpath(os.path.join(root,file),temp_dir)))
 
 shutil.rmtree(temp_dir)
