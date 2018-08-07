23ac3f2fb2baa915dba4e7a7ad8900b3d30ae90d
digitalohm <>
2018-08-07 10:18:17 -0700

    added comments
    comments for clarity

--- a8_generator.py
+++ a8_generator.py
@@ -1,11 +1,14 @@
 import glob, os, re
 
+## Voltage table for the nerseq.  This is for notes C-4 through G-4 to hit Zones 8 thru 1 (in that order)
 vt_nerdseq_001 = ['+4.56', '+4.48', '+4.39', '+4.31', '+4.23', '+4.14', '+4.04', '-5.00']
 
+## This is the name of the preset which works nicely if it's the same name as your sample source folder
 preset = "prst003"
 
 sample_dir = '/Users/digitalohm/Documents/' + preset
 
+## First we build a list of the channel directories that are not empty, and print the beginnings of the yml
 channels = []
 print('Preset 1 :')
 print('  Name : ' + sample_dir[len(sample_dir)-7:len(sample_dir)])
@@ -15,8 +18,7 @@ for root, dirnames, filenames in os.walk(sample_dir):
         if os.listdir(sample_dir + '/' + directory) :
             channels.append(directory)
 
-#print(channels)
-
+## Next we loop through each folder from the channles[] list and print the yml
 i = 1
 for directory in channels:
     samples = []
@@ -42,5 +44,6 @@ for directory in channels:
         print('      Sample : ' + sample)
         print('      MinVoltage : ' + vt_nerdseq_001[j])
         j += 1
+        
     del sample
     i += 1
